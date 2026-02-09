d156b5f1753a17b2b480bc8ad2486cb00e67b0c7
Omar Mohamed <omartemhosma7@gmail.com>
2026-02-09 09:27:47 +0200

    Add support for inverse and hyperbolic trigonometric functions in TrigNativeFunction

--- src/main/java/me/abdelaziz/runtime/function/nat/math/TrigNativeFunction.java
+++ src/main/java/me/abdelaziz/runtime/function/nat/math/TrigNativeFunction.java
@@ -18,33 +18,35 @@ public final class TrigNativeFunction extends NativeFunction {
         final double v = value.asDouble();
 
         switch (type) {
-            case SIN:
-                return new Value(Math.sin(v));
-            case COS:
-                return new Value(Math.cos(v));
-            case TAN:
-                return new Value(Math.tan(v));
+            case SIN:   return new Value(Math.sin(v));
+            case COS:   return new Value(Math.cos(v));
+            case TAN:   return new Value(Math.tan(v));
 
-            case ASIN:
-                return new Value(Math.asin(v));
-            case ACOS:
-                return new Value(Math.acos(v));
-            case ATAN:
-                return new Value(Math.atan(v));
+            case ASIN:  return new Value(Math.asin(v));
+            case ACOS:  return new Value(Math.acos(v));
+            case ATAN:  return new Value(Math.atan(v));
 
-            case SINH:
-                return new Value(Math.sinh(v));
-            case COSH:
-                return new Value(Math.cosh(v));
-            case TANH:
-                return new Value(Math.tanh(v));
+            case SINH:  return new Value(Math.sinh(v));
+            case COSH:  return new Value(Math.cosh(v));
+            case TANH:  return new Value(Math.tanh(v));
 
-            case SEC:
-                return new Value(1.0 / Math.cos(v));
-            case CSC:
-                return new Value(1.0 / Math.sin(v));
-            case COT:
-                return new Value(1.0 / Math.tan(v));
+            case ASINH: return new Value(Math.log(v + Math.sqrt(v * v + 1.0)));
+
+            case ACOSH: return new Value(Math.log(v + Math.sqrt(v * v - 1.0)));
+
+            case ATANH: return new Value(0.5 * Math.log((1.0 + v) / (1.0 - v)));
+
+            case SEC:   return new Value(1.0 / Math.cos(v));
+            case CSC:   return new Value(1.0 / Math.sin(v));
+            case COT:   return new Value(1.0 / Math.tan(v));
+
+            case ASEC:  return new Value(Math.acos(1.0 / v));
+            case ACSC:  return new Value(Math.asin(1.0 / v));
+            case ACOT:  return new Value(Math.atan(1.0 / v));
+
+            case SECH:  return new Value(1.0 / Math.cosh(v));
+            case CSCH:  return new Value(1.0 / Math.sinh(v));
+            case COTH:  return new Value(1.0 / Math.tanh(v));
 
             default:
                 throw new IllegalStateException("Unhandled trig type: " + type);
@@ -52,6 +54,7 @@ public final class TrigNativeFunction extends NativeFunction {
     }
 
     public enum TrigType {
+
         SIN("sin"),
         COS("cos"),
         TAN("tan"),
@@ -64,9 +67,21 @@ public final class TrigNativeFunction extends NativeFunction {
         COSH("cosh"),
         TANH("tanh"),
 
+        ASINH("asinh"),
+        ACOSH("acosh"),
+        ATANH("atanh"),
+
         SEC("sec"),
         CSC("csc"),
-        COT("cot");
+        COT("cot"),
+
+        ASEC("asec"),
+        ACSC("acsc"),
+        ACOT("acot"),
+
+        SECH("sech"),
+        CSCH("csch"),
+        COTH("coth");
 
         private final String name;
 
